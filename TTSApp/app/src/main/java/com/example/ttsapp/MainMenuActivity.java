80d432348cecc25c8ad2107df645018e1592f838
naffins <64361023+naffins@users.noreply.github.com>
2022-03-22 02:05:43 +0800

    Completed main menu

--- TTSApp/app/src/main/java/com/example/ttsapp/MainMenuActivity.java
+++ TTSApp/app/src/main/java/com/example/ttsapp/MainMenuActivity.java
@@ -2,6 +2,7 @@ package com.example.ttsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,14 +17,33 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        // Assign exit button
+        // Assign buttons
+        viewListButton = findViewById(R.id.activity_main_menu_student_list_button);
+        modifyListButton = findViewById(R.id.activity_main_menu_modify_student_list_button);
         exitButton = findViewById(R.id.activity_main_menu_exit_button);
+
+        // Add onClick listener for view list button
+        viewListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start view student list activity to list students
+                startActivity(new Intent(MainMenuActivity.this,ViewStudentListActivity.class));
+            }
+        });
+
+        // Add onClick listener for modify list button
+        modifyListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start modify student list activity to modify student list
+                startActivity(new Intent(MainMenuActivity.this,ModifyStudentListActivity.class));
+            }
+        });
 
         // Add onClick listener for exit button
         exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 // Return to connect activity
                 finish();
             }
---
+++ TTSApp/app/src/main/java/com/example/ttsapp/ModifyStudentListActivity.java
@@ -0,0 +1,14 @@
+package com.example.ttsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModifyStudentListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_student_list);
+    }
+}
