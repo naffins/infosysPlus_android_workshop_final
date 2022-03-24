a650a610c1e6c48590a3ab3a918ad51861e9b546
naffins <64361023+naffins@users.noreply.github.com>
2022-03-24 14:33:19 +0800

    Implemented working app

---
+++ StudentRegistry/app/src/main/java/com/example/studentregistry/RequestHelper.java
@@ -0,0 +1,154 @@
+package com.example.studentregistry;
+
+
+import android.os.Handler;
+import android.util.Log;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+// TODO: Complete this class (to be guided)
+public class RequestHelper extends Thread {
+
+    public static interface PostRequestTask {
+
+        // TO REMOVE IN STARTER CODE
+        public void postRequestExecute(String jsonOutput, int responseCode);
+    }
+
+    private static String ipAddress = null;
+
+    public static void setIpAddress(String ipAddress) {
+        RequestHelper.ipAddress = ipAddress;
+    }
+
+    // =====START OF CODE TO BE REMOVED IN STARTER VERSION=====
+
+    private final String path;
+    private final String jsonInput;
+    private final String method;
+    private volatile String jsonOutput;
+    private volatile int responseCode = -1;
+    private PostRequestTask postRequestTask;
+    private Handler handler;
+
+    public RequestHelper(String path, String jsonInput, String method, PostRequestTask postRequestTask) {
+        this.path = path;
+        this.jsonInput = jsonInput;
+        this.method = method;
+        this.postRequestTask = postRequestTask;
+        this.handler = new Handler();
+    }
+
+    // Thread's main program flow
+    public void run() {
+
+        OutputStream outputStream = null;
+        OutputStreamWriter outputStreamWriter = null;
+        BufferedWriter writer = null;
+        InputStream inputStream = null;
+        InputStreamReader inputStreamReader = null;
+        BufferedReader reader = null;
+
+        try {
+
+            // Setup connection to URL
+            HttpURLConnection connection = (HttpURLConnection) new URL("http://"+ipAddress+":8000"+path).openConnection();
+
+            // Indicate method
+            connection.setRequestMethod(method);
+
+            // Indicate that we accept JSON as reply
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setDoInput(true);
+
+            if ((jsonInput!=null)&&(!method.equals("GET"))) {
+                // Indicate content type as JSON
+                connection.setRequestProperty("Content-Type", "application/json; utf-8");
+                // Allow sending of content/receipt of content
+                connection.setDoOutput(true);
+
+                outputStream = new BufferedOutputStream(connection.getOutputStream());
+                outputStreamWriter = new OutputStreamWriter(outputStream, "UTF-8");
+
+                writer = new BufferedWriter(outputStreamWriter);
+                writer.write(jsonInput);
+                writer.flush();
+                writer.close();
+                writer = null;
+                outputStreamWriter.close();
+                outputStreamWriter = null;
+                outputStream.close();
+                outputStream = null;
+            }
+
+            connection.connect();
+
+            responseCode = connection.getResponseCode();
+
+            inputStream = connection.getInputStream();
+            StringBuffer output = new StringBuffer("");
+
+            if (inputStream != null) {
+                inputStreamReader = new InputStreamReader(inputStream);
+                reader = new BufferedReader(inputStreamReader);
+                String read = reader.readLine();
+                while (read != null) {
+                    output.append(read);
+                    read = reader.readLine();
+                }
+                reader.close();
+                reader = null;
+                inputStreamReader.close();
+                inputStreamReader = null;
+                inputStream.close();
+                inputStream = null;
+            }
+            jsonOutput = output.toString();
+        }
+        catch (Exception e) {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Log.e("PostHelper","Error: could not execute request");
+                    e.printStackTrace();
+                }
+            });
+            jsonOutput = null;
+
+            try {
+                if (outputStream!=null) outputStream.close();
+                if (outputStreamWriter!=null) outputStreamWriter.close();
+                if (writer!=null) writer.close();
+                if (inputStream!=null) inputStream.close();
+                if (inputStreamReader!=null) inputStreamReader.close();
+                if (reader!=null) reader.close();
+            }
+            catch (Exception f) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        f.printStackTrace();
+                        //Log.e("PostHelper", Arrays.toString(f.getStackTrace()));
+                    }
+                });
+            }
+        }
+
+        handler.post(new Runnable() {
+            public void run() {
+                postRequestTask.postRequestExecute(jsonOutput,responseCode);
+            }
+        });
+
+        // =====END OF CODE TO BE REMOVED IN STARTER VERSION=====
+    }
+}
+
--- api/main.py
+++ api/main.py
@@ -1,16 +1,20 @@
 # connect to mysql on CLI: mysql -h localhost -P 3306 -u root -D db -p
 # run this file: uvicorn main:app --reload
-
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, Response
+from pydantic import BaseModel
+import uvicorn
 import pymysql.cursors
 
 app = FastAPI()
+
+SQL_ERROR = {"error": "sql_error"}
 
 # create database connection
 connection = pymysql.connect(
     host="localhost",
     user="root",
-    password="root",
+    password="example",
     database="db",
     cursorclass=pymysql.cursors.DictCursor,
 )
@@ -18,52 +22,103 @@ connection = pymysql.connect(
 # "environment variables"
 table_name = "students"
 
-# basic route
-@app.get("/")
-async def root():
-    return "Hello world"
+# Route for ConnectActivity
 
-# put because this function is idempotent
-@app.put("/create_entry")
-async def create_entry(
-    student_id: int, name: str, year:int, is_undergraduate: bool, is_vaccinated: bool
-):
-    sql = f"INSERT INTO {table_name} (id, name, year, is_undergraduate, is_vaccinated) VALUES ({student_id}, \"{name}\", {year}, {is_undergraduate}, {is_vaccinated})"
+## Route
+@app.get("/")
+async def connectActivity():
+    return {"success": True}
+
+# Route for ViewStudentListActivity
+
+## Method
+@app.get("/student_list")
+async def viewStudentListActivity(response:Response, id: Optional[int] = None,
+    name: Optional[str] = None, year: Optional[int] = None, is_undergraduate: Optional[bool] = None,
+    is_vaccinated: Optional[bool] = None):
+
+    # Compile WHERE filter
+    filters = []
+    if id != None: filters.append("id={}".format(id))
+    if name != None: filters.append("name=\"{}\"".format(name))
+    if year != None: filters.append("year={}".format(year))
+    if is_undergraduate != None: filters.append("is_undergraduate={}".format(is_undergraduate))
+    if is_vaccinated != None: filters.append("is_vaccinated={}".format(is_vaccinated))
+
+    print(id,name,year,is_undergraduate,is_vaccinated)
+    print(filters)
+    filter = " AND ".join(filters)
+
+    # Compose query
+    sql = f"SELECT * FROM {table_name}{' WHERE ' + filter if len(filter)>0 else ''};"
 
     # closes the connection when done
-    with connection.cursor() as cursor:
+    with connection.cursor() as cursor: 
         try:
+            # Make query and return results
             cursor.execute(sql)
-        except:
-            return {"status": "FAILED", "message": "Could not execute SQL"}
+            reply = cursor.fetchall()
+            for i in reply:
+                i["is_undergraduate"] = i["is_undergraduate"]==1
+                i["is_vaccinated"] = i["is_vaccinated"]==1
+            print(reply)
+            return reply
+        except Exception as e:
+            response.status_code = 500
+            return SQL_ERROR
 
-    # save changes to DB (committing transaction)
+# Route for ModifyStudentListActivity add function
+
+## Template for POST request body
+class ModifyStudentListActivity_Add_Template(BaseModel):
+    id: int
+    name: str
+    year: int
+    is_undergraduate: int
+    is_vaccinated: int
+
+## Route
+@app.post("/add_student")
+async def modifyStudentListActivity_add(response: Response, student_data:ModifyStudentListActivity_Add_Template):
+
+    sql = f"INSERT INTO {table_name} (id, name, year, is_undergraduate, is_vaccinated) VALUES ({student_data.id}, \"{student_data.name}\", {student_data.year}, {student_data.is_undergraduate}, {student_data.is_vaccinated});"
+
     try:
+        with connection.cursor() as cursor:
+            cursor.execute(sql)
         connection.commit()
-    except:
-        return {"status": "FAILED", "message": "Could not commit transaction"}
+    except Exception as e:
+        connection.rollback()
+        if "Duplicate entry" in str(e):
+            response.status_code = 400
+            return {"error": "id_already_exists"}
+        response.status_code = 500
+        return SQL_ERROR
+    
+# Route for ModifyStudentListActivity delete function
 
-    return {"status": "OK"}
+## Route
+@app.delete("/student")
+async def ModifyStudentListActivity_delete(response: Response, id: int):
+    
+    # Compose query
+    sql = f"DELETE FROM {table_name} WHERE id = {id};"
 
-@app.get("/entry")
-async def get_entry(student_id: int = float('-inf')):
-    if student_id == float('-inf'):
-        # no ID provided, return all entries
-        sql = f"SELECT * FROM {table_name}"
-
+    try:
         with connection.cursor() as cursor:
-            try:
-                cursor.execute(sql)
-                return cursor.fetchall()
-            except:
-                return {"status": "FAILED", "message": "Could not execute SQL"}
-    else:
-        # id provided, return all matching entries if any
-        sql = f"SELECT * FROM {table_name} WHERE id = {student_id}"
+            cursor.execute(sql)
+            connection.commit()
 
-        with connection.cursor() as cursor:
-            try:
-                cursor.execute(sql)
-                return cursor.fetchone()
-            except:
-                return {"status": "FAILED", "message": "Could not execute SQL"}
+            # Check number of affected rows
+            if (cursor.rowcount==0):
+
+                # If no rows affected then ID didnt exist to begin with
+                response.status_code = 400
+                return {"error": "id_not_found"}
+            return {"success": True}
+    except Exception as e:
+        response.status_code = 500
+        return SQL_ERROR
+
+if __name__=="__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
